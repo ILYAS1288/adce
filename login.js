54f69c16b90183e0512958561c233f7b6143edcd
ILYAS1288 <155062024+ILYAS1288@users.noreply.github.com>
2024-07-09 20:04:53 +0500

    change

---
+++ login.js
@@ -0,0 +1,15 @@
+let newheading = document.createElement("h2")
+newheading. textContent ="well come";
+console.log(newheading);
+
+
+var newHeading = document.createElement("h2");
+
+newHeading.textContent = "New Heading";
+
+console.log(newHeading);
+
+
+
+
+
