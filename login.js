1fbf258183423c98538edf659d27d71ac3aa6c4e
ILYAS1288 <elibubalteen@gmail.com>
2024-07-10 19:40:26 +0500

    change

--- login.js
+++ login.js
@@ -1,15 +1,27 @@
-let newheading = document.createElement("h2")
-newheading. textContent ="well come";
-console.log(newheading);
+//let newheading = document.createElement("h2");
+//newheading. textContent ="well come";
+//console.log(newheading);
+//document.body.appendChild(newheading);
+
+//var newHeading = document.createElement("h2");
+
+//newHeading.textContent = "New Heading";
+
+//console.log(newHeading);
+
+//let secondH = document.createElement("h3");
+//secondH.innerHTML = "This is a dynamically created <strong>h3</strong> element.";
 
 
-var newHeading = document.createElement("h2");
-
-newHeading.textContent = "New Heading";
-
-console.log(newHeading);
 
 
+let heading = document.createElement("h1");
+heading.textContent = "welcome to my website";
+document.body.appendChild(heading);
+heading.style.cssText= "color:blue; font-size:35px; margin-left:400px";
 
 
-
+ let para = document.createElement("p");
+ para.textContent = "hello , world ksjdcnask csak heleo for your teame aya nskdns";
+ document.body.appendChild(para);
+ para.style="font-size:21px; color:red;"
