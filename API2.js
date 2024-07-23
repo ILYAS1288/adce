556ed361d058b8ff6477e172637c4a64db9803d5
ILYAS1288 <elibubalteen@gmail.com>
2024-07-24 01:55:15 +0500

    API12

---
+++ API2.js
@@ -0,0 +1,47 @@
+// Step 1:
+
+
+// Step 2:
+const container = document.querySelector(".container");
+
+// Step 3: 
+const url = "https://fakestoreapi.com/products";
+
+fetch(url)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(`Error: ${res.status} ${res.statusText}`);
+    }
+  })
+  .then((products) => {
+   
+    const mensCategory = products.filter((product) => product.category === "men's clothing");
+
+    
+    console.log("Filtered Men's Category Products:", mensCategory);
+
+    mensCategory.forEach((product) => {
+      console.log(product); 
+    });
+
+    //  DOM (creating elements)
+    mensCategory.forEach((product) => {
+      const createDiv = document.createElement('div');
+      createDiv.classList.add("card");
+      createDiv.innerHTML = `
+        <div class="img">
+          <img src="${product.image}" alt="${product.title}">
+        </div>
+        <div class="content">
+          <h4>${product.title}</h4>
+          <p>Price ${product.price} PKR</p>
+        </div>
+      `;
+      container.appendChild(createDiv); // Append 
+    });
+  })
+  .catch((err) => {
+    console.error("Error fetching data:", err);
+  });
