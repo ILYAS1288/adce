720509a3f376ac68e2fa74b1a0c0bb250a6e2620
ILYAS1288 <elibubalteen@gmail.com>
2024-07-25 02:41:48 +0500

    API12

--- API2.js
+++ API2.js
@@ -18,8 +18,6 @@ fetch(url)
   .then((products) => {
    
     const mensCategory = products.filter((product) => product.category === "men's clothing");
-
-    
     console.log("Filtered Men's Category Products:", mensCategory);
 
     mensCategory.forEach((product) => {
@@ -39,9 +37,55 @@ fetch(url)
           <p>Price ${product.price} PKR</p>
         </div>
       `;
+
+
       container.appendChild(createDiv); 
     });
   })
   .catch((err) => {
     console.error("Error fetching data:", err);
   });
+
+  const container1 = document.querySelector(".container1");
+
+  // Step 3: 
+  const url1 = "https://fakestoreapi.com/products";
+  
+  fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(`Error: ${res.status} ${res.statusText}`);
+      }
+    })
+    .then((products) => {
+      // Filter products based on electronics category
+      const electronicsCategory = products.filter((product) => product.category === "electronics");
+      
+      console.log("Filtered electronicsCategory Products:", electronicsCategory);
+  
+      electronicsCategory.forEach((product) => {
+        console.log(product); 
+        // Create DOM elements for each product
+        const createDiv = document.createElement('div');
+        createDiv.classList.add("card1");
+        createDiv.innerHTML = `
+          <div class="img1">
+            <img src="${product.image}" alt="${product.title}">
+          </div>
+          <div class="content1">
+            <h4>${product.title}</h4>
+            <p>Price ${product.price} PKR</p>
+          </div>
+        `;
+        container1.appendChild(createDiv); 
+      });
+    })
+    .catch((err) => {
+      console.error("Error fetching data:", err);
+    });
+  
+
+
+  
